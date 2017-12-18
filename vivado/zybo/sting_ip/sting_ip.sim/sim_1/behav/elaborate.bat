efcebd564d4b9a5271c37238a422465b17e46a99
natsutan <YRW03704@nifty.com>
2017-12-18 19:54:55 +0900

    新しいテストパターン追加

--- vivado/zybo/sting_ip/sting_ip.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto e114580bb8f94c17800037a6e5f74a91 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L axi_infrastructure_v1_1_0 -L xil_common_vip_v1_0_0 -L smartconnect_v1_0 -L axi_protocol_checker_v1_1_14 -L axi_vip_v1_0_2 -L work -L unisims_ver -L unimacro_ver -L secureip -L xpm --snapshot sim1_tb_behav work.sim1_tb work.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
---
+++ vivado/zybo/sting_ip/sting_ip.srcs/unit_test/new/weight_read.v
@@ -0,0 +1,98 @@
+`timescale 1ns/1ps
+
+import axi_vip_v1_0_2_pkg::*;
+import design_1_axi_vip_0_1_pkg::*;
+
+`include "sting_reg.h"
+    
+module tb_top();
+
+    localparam int LP_CLK_PERI = 10;
+    localparam int LP_RST_PERI = 50;
+
+    // DUT instance
+    logic aresetn, aclk;
+    logic axi_init;
+    wire irq;
+   integer rd_data;
+   
+ 
+   design_1 design_1(
+    .aclk(aclk),
+    .aresetn(aresetn),
+    .init_axi_txn(axi_init),
+    .irq(irq));
+    
+    task rst_gen();
+        aresetn = '0;
+        axi_init = '1;
+        #(LP_RST_PERI);
+        axi_init = '0;
+        aresetn = '1;
+    endtask
+
+    task clk_gen();
+        aclk = '0;
+        forever #(LP_CLK_PERI/2) aclk = ~aclk;
+    endtask
+
+    task clk_dly(int n);
+        repeat(n) @(posedge aclk);
+    endtask
+
+    // VIP decreation
+   design_1_axi_vip_0_1_mst_t  design_1_axi_vip_0_1_mst;
+   
+   initial begin 
+      design_1_axi_vip_0_1_mst = new("design_1_axi_vip_0_1_mst", tb_top.design_1.axi_vip_0.inst.IF);
+      design_1_axi_vip_0_1_mst.start_master();
+   end
+   
+   task reg_wr(input integer  adr,  input integer data);
+      axi_transaction  wr_transaction;
+      wr_transaction   = design_1_axi_vip_0_1_mst.wr_driver.create_transaction( "register write");
+      WR_TRANSACTION_FAIL: assert(wr_transaction.randomize());
+      wr_transaction.set_addr(adr);
+      wr_transaction.set_data_block(data);
+
+      design_1_axi_vip_0_1_mst.wr_driver.send(wr_transaction);
+   endtask 
+
+ 
+   
+    // Testscenario
+    initial begin
+       
+        fork
+           clk_gen();
+           rst_gen();
+        join_none
+
+       clk_dly(100);
+
+       reg_wr(`REG_CTRL, `REG_CTRL_RESET);
+       reg_wr(`REG_CTRL, `REG_CTRL_RUN);
+       
+       reg_wr(`REG_MODE, `REG_MODE_DIVMODE);
+       reg_wr(`REG_MODE, `REG_MODE_BNEN);
+       reg_wr(`REG_MODE, `REG_MODE_LRELUEN);
+
+       reg_wr(`REG_IXSIZE, 128);
+       reg_wr(`REG_IYSIZE, 1024);
+       reg_wr(`REG_ISADR,  32'h80000000);
+       reg_wr(`REG_IFSIZE, 32'h80010000);
+       reg_wr(`REG_WSADR1, 32'h80020000);
+       reg_wr(`REG_WSADR2, 32'h80030000);
+       reg_wr(`REG_OSADR, 32'h90000000);
+       reg_wr(`REG_OXSIZE, 64);       
+       reg_wr(`REG_OFSIZE,  500);
+
+       reg_wr(`REG_LRELU, 32'h12345678);
+       reg_wr(`REG_FNSIZE, 32'h02000300);
+
+       
+        clk_dly(1000);
+        $finish(2);
+    end
+endmodule
+
